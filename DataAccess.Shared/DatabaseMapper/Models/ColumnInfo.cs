b4e44a65ddd79093bf200e3692a3efc0815ba481
Avi Stokar <avistokar@outlook.com>
2023-01-26 16:37:08 -0500

    get type for filter and fix quotes when string

--- DataAccess.Shared/DatabaseMapper/Models/ColumnInfo.cs
+++ DataAccess.Shared/DatabaseMapper/Models/ColumnInfo.cs
@@ -1,20 +1,21 @@
 ﻿namespace DataAccess.Shared.DatabaseMapper.Models;
 
-public class ColumnInfo
-{
+public class ColumnInfo {
     public string ColumnName { get; init; }
     public string PropertyName { get; init; }
     public bool IsSkipByDefault { get; init; }
     public bool CanWrite { get; init; }
     public bool IsPrimaryKey { get; init; }
+    public Type Type { get; init; }
     public string Alias => PropertyName.Equals(ColumnName, StringComparison.InvariantCultureIgnoreCase) ? string.Empty : $" {PropertyName}";
 
-    public ColumnInfo(string columnName, string? propertyName = null, bool? isIsSkipByDefault = false, bool? canWrite = true, bool? isPrimaryKey = false)
-    {
+    public ColumnInfo(string columnName, string? propertyName = null, bool? isIsSkipByDefault = false, bool? canWrite = true,
+        bool? isPrimaryKey = false, Type? type = null) {
         ColumnName = columnName;
         PropertyName = propertyName ?? ColumnName;
         IsSkipByDefault = isIsSkipByDefault ?? false;
         CanWrite = canWrite ?? true;
         IsPrimaryKey = isPrimaryKey ?? false;
+        Type = type ?? typeof(string);
     }
 }
--- DataAccess.Shared/FilterService/FilterExpression.cs
+++ DataAccess.Shared/FilterService/FilterExpression.cs
@@ -1,15 +1,17 @@
 ﻿using System.Linq.Expressions;
+using System.Security.Cryptography;
 using System.Text.Json;
 using DataAccess.Shared.Helpers;
 
 namespace DataAccess.Shared.FilterService;
 
-public record FilterExpression(string PropertyName, Operator Operator, string? ColumnName = null) {
+public record FilterExpression(string PropertyName, Operator Operator, string? ColumnName = null, bool IsString = false) {
     public string PropertyName { get; protected set; } = PropertyName;
     public Operator Operator { get; protected set; } = Operator;
     public string? ColumnName { get; set; } = ColumnName ?? PropertyName;
+    public bool IsString { get; set; } = IsString;
 
-    public override string ToString() => $"{ColumnName ?? PropertyName} {Operator.DisplayName} {Operator.PreTemplate}@{PropertyName}{Operator.PostTemplate}";
+    public override string ToString() => $"{ColumnName ?? PropertyName} {Operator.DisplayName} {(IsString ? "'" : "")}{Operator.PreTemplate}@{PropertyName}{Operator.PostTemplate}{(IsString ? "'" : "")}";
 
     public static bool TryParse(string value, out FilterExpression? result) {
         result = JsonSerializer.Deserialize<FilterExpression>(value);
@@ -19,11 +21,22 @@ public record FilterExpression(string PropertyName, Operator Operator, string? C
 
 public record FilterExpression<T> : FilterExpression {
     public FilterExpression(string propertyName, Operator oper, string? columnName = null) : base(propertyName, oper, columnName) {
-      if (columnName is null && typeof(T).GetProperty(propertyName) is null) throw new ArgumentException($"Property: {propertyName} NOT found on {typeof(T).Name}");
+      var propertyInfo = typeof(T).GetProperty(propertyName);
+      if (propertyInfo is null) throw new ArgumentException($"Property: {propertyName} NOT found on {typeof(T).Name}");
+      IsString = propertyInfo.PropertyType == typeof(string);
     }
 
-    public FilterExpression(Expression<Func<T,object>> propertyName, Operator oper) : base(MemberHelpers.GetMemberName(propertyName), oper){ }
+    public FilterExpression(Expression<Func<T,object>> propertyNameExpression, Operator oper) : this(MemberHelpers.GetMemberName(propertyNameExpression), oper){ }
 
-    public override string ToString() => $"{ColumnName ?? PropertyName} {Operator.DisplayName} {Operator.PreTemplate}@{PropertyName}{Operator.PostTemplate}";
+    public override string ToString() {
+        var (pre, post) = stringifyTemplates();
+        return $"{ColumnName ?? PropertyName} {Operator.DisplayName} {pre}@{PropertyName}{post}";
 
+        (string pre, string post) stringifyTemplates() {
+            if (!IsString) return ("", "");
+            var before = string.IsNullOrWhiteSpace(Operator.PreTemplate) ? "" : $"'{Operator.PreTemplate}' + ";
+            var after = string.IsNullOrWhiteSpace(Operator.PostTemplate) ? "" : $" + '{Operator.PreTemplate}'";
+            return (before, after);
+        }
+    }
 }
