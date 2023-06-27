b16a7260482c64d56e0476a6ee2df2b01ebc1f87
stokara <avistokar@gmail.com>
2023-06-27 12:02:58 -0400

    fixed methods to use interface instead of concrete implementations

--- DataAccess/Services/DataService.cs
+++ DataAccess/Services/DataService.cs
@@ -6,11 +6,11 @@ using Microsoft.Extensions.Logging;
 namespace DataAccess;
 
 public abstract class DataService : IDataService {
-    private readonly DbConnectionManager connectionManager;
+    private readonly IDbConnectionManager connectionManager;
     private readonly IDatabaseMapper databaseMapper;
     private readonly ILoggerFactory loggerFactory;
 
-    protected DataService(DbConnectionManager connectionManager, IDatabaseMapper databaseMapper, ILoggerFactory loggerFactory) {
+    protected DataService(IDbConnectionManager connectionManager, IDatabaseMapper databaseMapper, ILoggerFactory loggerFactory) {
         this.connectionManager = connectionManager;
         this.databaseMapper = databaseMapper;
         this.loggerFactory = loggerFactory;
