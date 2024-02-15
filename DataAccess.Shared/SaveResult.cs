999e287818f8c0630010f2d8967bac2cab4f65d7
stokara <avistokar@gmail.com>
2024-02-15 10:58:51 -0500

    SaveResult inherits from Response renamed SaveResult to SaveResponse to indicate inheritence Writes return SaveResponse Queries retrun Response

--- DataAccess.Shared/SaveResult.cs
+++ DataAccess.Shared/SaveResult.cs
@@ -4,4 +4,8 @@ namespace DataAccess.Shared;
 
 public record SaveResult(int UpdatedCount, int DeletedCount, IdPk[] InsertedIds, Exception? Exception = null) {
     public bool Success => Exception is null;
+
+
+    public static SaveResult Failed(string exceptionMessage) => 
+        new(0, 0, [], new InvalidOperationException(exceptionMessage));
 }
