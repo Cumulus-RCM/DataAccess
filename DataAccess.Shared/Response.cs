85b467a3258720666c69e3ad544267001587edb6
stokara <avistokar@gmail.com>
2024-03-27 15:39:01 -0400

    updated response class and unit tests which broke becuase of changes to Response

--- DataAccess.Shared/Response.cs
+++ DataAccess.Shared/Response.cs
@@ -7,19 +7,29 @@ namespace DataAccess.Shared;
 
 public record Response(Exception? Exception = null) {
     public bool IsSuccess => Exception is null;
-
-    public static Response Failed(string exceptionMessage) => 
-        new Response(new InvalidOperationException(exceptionMessage));
 }
 
-public record Response<T>(IReadOnlyCollection<T> Items, int TotalCount = 0, string ErrorMessage = "") : Response {
+public record Response<T> : Response {
+    public IReadOnlyCollection<T> Items { get; init; }
+    public int TotalCount { get; init; }
+
     public Response() : this(new List<T>().AsReadOnly()) {}
-
     public Response(T value) : this(value.ItemAsReadOnlyCollection(), value.IsNullOrZero() ? 0 : 1, value.IsNullOrZero() ? "value can not be null." : "") { }
+    public Response(IReadOnlyCollection<T> items, int totalCount = 0, string? errorMessage = null, Exception? exception = null) {
+        this.Items = items;
+        this.TotalCount = totalCount;
+        Exception = exception;
+        if (!string.IsNullOrWhiteSpace(errorMessage) && exception is null) Exception = new InvalidOperationException(errorMessage);
+    }
 
-    public static Response<T> Empty(string errorMessage = "") => new(new List<T>().AsReadOnly(), ErrorMessage: errorMessage);
+    private Response(Exception? exception = null) : base(exception) {
+        Items = new List<T>().AsReadOnly();
+    }
 
-    public static Response<T> Fail(string errorMessage) => new(new List<T>().AsReadOnly(), ErrorMessage: errorMessage);
+    public static Response<T> Empty(string errorMessage = "") => new(new List<T>().AsReadOnly(), errorMessage: errorMessage);
+
+    public static Response<T> Fail(string errorMessage) => new Response<T>(new InvalidOperationException(errorMessage));
+    public static Response<T> Fail(Exception ex) => new(ex);
 
     public T GetItem() => Items.Single();
 }
