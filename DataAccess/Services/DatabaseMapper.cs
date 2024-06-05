46e14df6eed97cb0a45171cb644a8512785980b8
stokara <avistokar@gmail.com>
2024-06-05 11:56:49 -0400

    refactored DatabaseMapper.GetTableInfo - common code to private method

--- DataAccess/Services/DatabaseMapper.cs
+++ DataAccess/Services/DatabaseMapper.cs
@@ -17,29 +17,20 @@ public class DatabaseMapper : IDatabaseMapper {
     }
 
     public ITableInfo GetTableInfo<T>() {
-        ITableInfo newTableInfo;
-
         var type = typeof(T);
-        if (type.IsGenericType) {
-            type = type.GenericTypeArguments[0];
-            if (tableInfos.TryGetValue(type, out var value)) return value;
-            var ti = Activator.CreateInstance(typeof(TableInfo<>).MakeGenericType(type)) ?? 
-                     throw new InvalidOperationException($"Could not create TableInfo<{type}>");
-            newTableInfo = (ITableInfo) ti;
-        }
-        else {
-            if (tableInfos.TryGetValue(type, out var value)) return value;
-            newTableInfo = new TableInfo<T>();
-        }
-        tableInfos.Add(type, newTableInfo);
-        return  newTableInfo;
+        if (type.IsGenericType) type = type.GenericTypeArguments[0]; //ICollection<T>
+        return getTableInfo(type, ()=> new TableInfo<T>());
     }
 
-    public ITableInfo GetTableInfo(Type entityType) {
-        if (tableInfos.TryGetValue(entityType, out var value)) return value;
-        var newTableInfo = Activator.CreateInstance(typeof(TableInfo<>).MakeGenericType(entityType)) ??
-                           throw new InvalidOperationException($"Could not create TableInfo<{entityType}>");
-        tableInfos.Add(entityType, (ITableInfo)newTableInfo);
-        return (ITableInfo)newTableInfo;
+    public ITableInfo GetTableInfo(Type type) {
+        if (type.IsGenericType) type = type.GenericTypeArguments[0]; 
+        return getTableInfo(type, ()=> (ITableInfo) Activator.CreateInstance(typeof(TableInfo<>).MakeGenericType(type))!);
+    }
+
+    private ITableInfo getTableInfo(Type type, Func<ITableInfo> activator) {
+        if (tableInfos.TryGetValue(type, out var value)) return value;
+        var newTableInfo = activator() ?? throw new InvalidOperationException($"Could not create TableInfo<{type}>");
+        tableInfos.Add(type, newTableInfo);
+        return newTableInfo;
     }
 }
