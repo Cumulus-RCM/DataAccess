edc68c36b191ce58028ad20867475b4b0b298f02
stokara <avistokar@outlook.com>
2024-07-03 10:08:13 -0400

    cpt and diagnosis search using updated framework

--- DataAccess.Shared/Interfaces/IReader.cs
+++ DataAccess.Shared/Interfaces/IReader.cs
@@ -5,7 +5,6 @@ namespace DataAccess.Shared;
 
 public interface IReader<T> {
     Task<T?> GetByPkAsync(string pk);
-    Task<IReadOnlyCollection<T>> GetAllByPkAsync(IEnumerable<string> pkValues);
     Task<int> GetCountAsync(Filter? filter = null);
     Task<IReadOnlyCollection<T>> GetAllAsync(Filter? filter = null, int pageSize = 0, int pageNum = 0, OrderBy? orderBy = null);
     Task<IReadOnlyCollection<dynamic>> GetAllDynamicAsync(IReadOnlyCollection<string> columns, Filter? filter = null, int pageSize = 0, int pageNum = 0, OrderBy? orderBy = null);
