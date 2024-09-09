67c1b3fe9770615c053bb345b1d230d6f682a7bf
stokara <avistokar@gmail.com>
2024-09-09 17:21:30 -0400

    Refactor ReaderFactory and BillingDataReaderFactory
    Introduced ReaderFactory class in ReaderFactory.cs implementing
    IReaderFactory interface. Refactored BillingDataReaderFactory
    to inherit from ReaderFactory, simplifying its structure.
    Removed redundant methods from BillingDataReaderFactory,
    leveraging base class functionality for custom readers.

---
+++ DataAccess/ReaderFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using BaseLib;
+using DataAccess.Shared;
+
+namespace DataAccess;
+
+public class ReaderFactory : IReaderFactory{
+    private readonly IDbConnectionManager connectionManager;
+    private readonly IDatabaseMapper databaseMapper;
+    protected Lazy<Dictionary<Type, Type>> customReaders = new();
+
+    public ReaderFactory(IDbConnectionManager connectionManager, IDatabaseMapper databaseMapper) {
+        this.connectionManager = connectionManager;
+        this.databaseMapper = databaseMapper;
+    }
+
+    public IReader<T> GetReader<T>() where T : class {
+        var isCustom = customReaders.Value.TryGetValue(typeof(T), out var customReaderType);
+        var reader= isCustom
+            ? (Activator.CreateInstance(customReaderType!, connectionManager, databaseMapper) as IReader<T>)!
+            : new Reader<T>(connectionManager, databaseMapper);
+        return reader;
+    }
+
+    protected Dictionary<Type, Type> getCustomQueriesFromAssembly(Assembly assembly) {
+        var types = assembly.GetTypes();
+        var custom = types.Where(t => typeof(ICustomQueries).IsAssignableFrom(t) && !t.IsInterface).ToList();
+        return custom.Select(t => new KeyValuePair<Type,Type>(t.BaseType!.GetGenericArguments()[0], t)).ToDictionary(x=>x.Key, x=>x.Value);
+    }
+}
