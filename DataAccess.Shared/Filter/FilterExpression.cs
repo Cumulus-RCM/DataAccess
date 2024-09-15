be3bf7bd9eacc62e83224974676cde0661b583e1
stokara <avistokar@gmail.com>
2024-09-14 21:43:08 -0400

    added Filter ctor which takes Linq expression

--- DataAccess.Shared/Filter/FilterExpression.cs
+++ DataAccess.Shared/Filter/FilterExpression.cs
@@ -6,7 +6,8 @@ namespace DataAccess.Shared;
 
 public record FilterExpression(string PropertyName, Operator Operator) {
     private object? val;
-    public object? Value { 
+
+    public object? Value {
         get => val;
         set {
             val = value;
@@ -36,7 +37,7 @@ public record FilterExpression<T> : FilterExpression {
     public FilterExpression(string propertyName, Operator op, object? value = null) : base(propertyName, op) {
         //ensure property exists on T
         _ = typeof(T).GetProperty(propertyName) ??
-                           throw new ArgumentException($"Property: {propertyName} NOT found on {typeof(T).Name}");
+            throw new ArgumentException($"Property: {propertyName} NOT found on {typeof(T).Name}");
         if (value is not null) Value = value;
     }
 
@@ -45,16 +46,46 @@ public record FilterExpression<T> : FilterExpression {
     public static bool TryParse(string value, out FilterExpression<T>? result) {
         result = null;
         if (string.IsNullOrWhiteSpace(value)) return false;
-        result = JsonSerializer.Deserialize<FilterExpression<T>>(value);
-        if (result is not null && result.Value is JsonElement jsonElement) {
-            var type = Type.GetType(result.ValueTypeName);
-            if (type is not null) result.Value = JsonSerializer.Deserialize(jsonElement.GetRawText(), type);
+        try {
+            result = JsonSerializer.Deserialize<FilterExpression<T>>(value);
+            if (result?.Value is JsonElement jsonElement) {
+                var type = Type.GetType(result.ValueTypeName);
+                if (type is not null) result.Value = JsonSerializer.Deserialize(jsonElement.GetRawText(), type);
+            }
+
+            return result is not null;
         }
-        return result is not null;
+        catch (Exception ex) {
+            return false;
+        }
+        
+            
     }
 
     public FilterExpression(Expression<Func<T, object>> propertyNameExpression, Operator op, object? value = null) :
         this(MemberHelpers.GetMemberName(propertyNameExpression), op, value) {
         if (value is not null) Value = value;
+    }
+
+    public FilterExpression(Expression<Func<T, bool>> linqExpression) {
+        if (!(linqExpression.Body is BinaryExpression {Left: MemberExpression memberExpression} binaryExpression))
+            throw new ArgumentException("The expression body must be a BinaryExpression with a MemberExpression operand.", nameof(linqExpression));
+
+        Name = memberExpression.Member.Name;
+        Operator = binaryExpression.NodeType switch {
+            ExpressionType.Equal => Operator.Equal,
+            ExpressionType.NotEqual => Operator.NotEqual,
+            ExpressionType.GreaterThan => Operator.GreaterThan,
+            ExpressionType.GreaterThanOrEqual => Operator.GreaterThanOrEqual,
+            ExpressionType.LessThan => Operator.LessThan,
+            ExpressionType.LessThanOrEqual => Operator.LessThanOrEqual,
+            _ => throw new ArgumentException("Unsupported operator", nameof(linqExpression))
+        };
+
+        Value = binaryExpression.Right switch {
+            ConstantExpression constantExpression => constantExpression.Value,
+            MemberExpression valueExpression => Expression.Lambda(valueExpression).Compile().DynamicInvoke(),
+            _ => throw new ArgumentException("Invalid expression format", nameof(linqExpression))
+        };
     }
 }
