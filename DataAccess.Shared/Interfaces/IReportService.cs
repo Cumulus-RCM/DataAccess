3f7c40bf420d85d55db33a34d8135cdff8d517e2
stokara <avistokar@gmail.com>
2024-09-24 22:20:32 -0400

    Add filter params to methods and new export to Excel method
    Modified `GetReportDataAsync` and `GetCountAsync` to include an optional `Filter` parameter. Added `ExportReportToExcelAsync` method to handle exporting reports to Excel with specified parameters.

--- DataAccess.Shared/Interfaces/IReportService.cs
+++ DataAccess.Shared/Interfaces/IReportService.cs
@@ -6,6 +6,7 @@ namespace DataAccess.Shared;
 public interface IReportService {
     Task<Response<(string reportName, string reportDescription)>> GetReportsAsync(bool forceRefresh = false);
     Task<Response<ReportDefinition>> GetReportDefinitionAsync(string reportName);
-    Task<Response<dynamic>> GetReportDataAsync(ReportDefinition reportDefinition, int pageSize = 0, int pageNum = 0);
-    Task<long> GetCountAsync(ReportDefinition reportDefinition);
+    Task<Response<dynamic>> GetReportDataAsync(ReportDefinition reportDefinition, Filter? filter, int pageSize = 0, int pageNum = 0);
+    Task<long> GetCountAsync(ReportDefinition reportDefinition, Filter? filter);
+    Task<Response> ExportReportToExcelAsync(ReportDefinition reportDefinition, Filter filter, string filename);
 }
