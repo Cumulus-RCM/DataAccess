adbb97119acdc64658a0c30bc1b7c3c144f7a47f
stokara <avistokar@gmail.com>
2024-10-06 09:03:03 -0400

    Refactor IQueries and IBillableServiceQueries methods
    Updated IQueries<T> and IBillableServiceQueries interfaces to include new overloads for methods that accept Filter objects and convert them to JSON. Removed original methods that directly accepted Filter objects to avoid redundancy. Enhanced GetBillableServicesWithLookupsAsync to handle nullable filterJson, improving robustness and flexibility. These changes standardize filter and parameter handling, ensuring consistency and simplifying API usage.

--- DataAccess.Shared/Interfaces/IQueries.cs
+++ DataAccess.Shared/Interfaces/IQueries.cs
@@ -11,6 +11,10 @@ public interface IQueries<T> {
     //NOTE: This is a generic interface for all queries. It is used to get data from the database.
     //NOTE: All parameters to interface methods must be serializable to JSON to allow them to be processed by the API.
 
+    Task<Response<T>> GetAllAsync(Filter? filter, int pageSize = 0, int pageNumber = 1, OrderBy? orderBy = null,
+        IReadOnlyCollection<string>? columnNames = null, ParameterValues? parameterValues = null) =>
+        GetAllAsync(filterJson: filter?.AsJson(), pageSize, pageNumber, orderBy?.AsJson(), columnNames, parameterValues?.AsJson());
+
     [Get("/")]
     Task<Response<T>> GetAllAsync(string? filterJson = null, int pageSize = 0, int pageNumber = 1, string? orderByJson = null,
         IReadOnlyCollection<string>? columnNames = null, string? parameterValues = null);
@@ -23,10 +27,6 @@ public interface IQueries<T> {
     Task<int> GetCountAsync(string? filterJson = null, string? parameterValues = null);
 
     Task<int> GetCountAsync(Filter? filter = null, ParameterValues? parameterValues = null) => GetCountAsync(filter?.AsJson(), parameterValues?.AsJson());
-
-    Task<Response<T>> GetAllAsync(Filter? filter, int pageSize = 0, int pageNumber = 1, OrderBy? orderBy = null,
-        IReadOnlyCollection<string>? columnNames = null, ParameterValues? parameterValues = null) =>
-        GetAllAsync(filterJson: filter?.AsJson(), pageSize, pageNumber, orderBy?.AsJson(), columnNames, parameterValues?.AsJson());
 
     [Get("/{pkValue}")]
     Task<Response<T>> GetByPkAsync(string pkValue, IReadOnlyCollection<string>? columnNames = null, string? parameterValues = null);
