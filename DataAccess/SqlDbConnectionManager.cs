e0527ac8e2f81245fb3fc417e197ae18e4b52edf
stokara <avistokar@outlook.com>
2024-10-16 10:44:18 -0400

    Refactor DbConnectionManager to be generic and flexible
     T is constrained to be a Component and IDbConnection.
    
    - Added OnConnectionDisposed method to handle connection disposal.

---
+++ DataAccess/SqlDbConnectionManager.cs
@@ -0,0 +1,8 @@
+﻿using BaseLib;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging;
+
+namespace DataAccess;
+
+public sealed class SqlDbConnectionManager(string connectionString, ILoggerFactory loggerFactory)
+    : DbConnectionManager<SqlConnection>(connectionString, loggerFactory);
