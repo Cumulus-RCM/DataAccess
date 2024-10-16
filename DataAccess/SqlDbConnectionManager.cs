136a6487937e7af807155e0fa60cbfee1bfc975f
stokara <avistokar@outlook.com>
2024-10-16 13:41:07 -0400

    Refactor connection management to use DbConnectionOptions
    Introduced DbConnectionOptions record for connection strings, for DI resolution
    Updated DbConnectionManager to use connectionString parameter.
    Added new constructor to SqlDbConnectionManager for DbConnectionOptions.
    Improves code clarity and structure by encapsulating connection details.

--- DataAccess/SqlDbConnectionManager.cs
+++ DataAccess/SqlDbConnectionManager.cs
@@ -5,4 +5,7 @@ using Microsoft.Extensions.Logging;
 namespace DataAccess;
 
 public sealed class SqlDbConnectionManager(string connectionString, ILoggerFactory loggerFactory)
-    : DbConnectionManager<SqlConnection>(connectionString, loggerFactory);
+    : DbConnectionManager<SqlConnection>(connectionString, loggerFactory) {
+    public SqlDbConnectionManager(DbConnectionOptions dbConnectionOptions, ILoggerFactory loggerFactory) 
+        : this(dbConnectionOptions.ConnectionString, loggerFactory) {}
+}
